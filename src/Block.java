6c1057fcc8f69bbae3df1eded57296661808bffd
Jeff Koenig <jkoenig@thinkthroughmath.com>
2014-02-09 22:21:28 -0500

    cleaning up tabs and spaces

--- src/Block.java
+++ src/Block.java
@@ -3,25 +3,25 @@ import javax.swing.JPanel;
 
 
 public class Block extends JPanel {
-	final int row, column;
-	private boolean lit=false;
+  final int row, column;
+  private boolean lit=false;
 
-	public Block(int column, int row){
-		this.column=column;
-		this.row=row;
-	}
+  public Block(int column, int row){
+    this.column=column;
+    this.row=row;
+  }
 
-	public void light(){
-		lit=true;
-		this.setBackground(Color.GREEN);
-	}
-	
-	public void unLight(){
-		lit=false;
-		this.setBackground(Color.LIGHT_GRAY);
-	}
-	
-	public boolean isLit(){
-		return lit;
-	}
+  public void light(){
+    lit=true;
+    this.setBackground(Color.GREEN);
+  }
+
+  public void unLight(){
+    lit=false;
+    this.setBackground(Color.LIGHT_GRAY);
+  }
+
+  public boolean isLit(){
+    return lit;
+  }
 }
